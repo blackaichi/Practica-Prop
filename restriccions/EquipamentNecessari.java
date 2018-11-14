db3d49904da19e680e45b29ae31380a40368a0a8
Eric Casanovas <ecasanovascantarero@gmail.com>
2018-11-14 15:05:14 +0100

    Add files via upload

--- restriccions/EquipamentNecessari.java
+++ restriccions/EquipamentNecessari.java
@@ -11,8 +11,31 @@ import classes.*;
 
 public class EquipamentNecessari {
 	
+	/**
+	 * Element al qual pertany
+	 */
 	private Element element;
 	
+	/**
+	 * Creadora de Equipament necessari amb element com a paràmetre
+	 * @param e
+	 * @throws Exception
+	 */
+	public EquipamentNecessari(Element e) throws Exception {
+		ExceptionManager.thrower(setElement(e));
+	}
+	
+	/**
+	 * Assigna un element a Equipament necessari
+	 * @param e un element
+	 * @return 0 si no hi han errors, altrament errors
+	 */
+	private int setElement(Element e) {
+		if (e == null) return 260;
+		element = e;
+		return 0;
+	}
+
 	/**
 	 * Retorna les aules adients per la sessió
 	 * @param a aules que podem assignar a la sessió
@@ -20,7 +43,7 @@ public class EquipamentNecessari {
 	 * @return les aules adients per la sessió
 	 * @throws Exception 
 	 */
-	public HashSet<Aula> aulesadients(HashSet<Aula> a, SessioGAssignada sGA) throws Exception {
+	public HashSet<Aula> aulesAdients(HashSet<Aula> a, SessioGAssignada sGA) throws Exception {
 		if (a == null) ExceptionManager.thrower(210); 
 		if (sGA == null) ExceptionManager.thrower(210);
 		HashSet <Aula> aux = a;
@@ -35,7 +58,7 @@ public class EquipamentNecessari {
 	 * @return les aules adients per la sessió
 	 * @throws Exception
 	 */
-	public HashSet<Aula> aulesadients(HashSet<Aula> a, SessioSGAssignada sSGA) throws Exception {
+	public HashSet<Aula> aulesAdients(HashSet<Aula> a, SessioSGAssignada sSGA) throws Exception {
 		if (a == null) ExceptionManager.thrower(210); 
 		if (sSGA == null) ExceptionManager.thrower(212);
 		HashSet <Aula> aux = a;
