12759a8f9d5d78a56b86f63cfe519354098fcb55
Eric Casanovas <ecasanovascantarero@gmail.com>
2018-11-15 20:37:51 +0100

    Add files via upload

--- drivers/DriverPlaEstudis.java
+++ drivers/DriverPlaEstudis.java
@@ -1,85 +1,24 @@
 package drivers;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.*;
 import classes.*;
 
 public class DriverPlaEstudis {
-	
-	private PlaEstudis PE1;
-	private PlaEstudis PE2;
-	private PlaEstudis PE3;
-	private Assignatura a;
-	private Assignatura b;
-	
-	
+
 	/**
-	 * Iniciem alguns plans d'estudis
-	 */
-	@Before
-	public void setUp() throws Exception {
-		PE1 = new PlaEstudis("UPC");
-		PE2 = new PlaEstudis("UAB");
-		PE3 = new PlaEstudis("UDA");
-	}
-	
-	/**
-	 * Comprovem que vagi bé la constructora
-	 */
-	@Test 
-	public void testconstructora() throws Exception {
-		PlaEstudis PE = new PlaEstudis("fib");
-	}
-	
-	/**
-	 * Comprovem que salti error en cas de no passar-li nom
-	 */
-	@Test (expected = Exception.class)	public void testErrorConstructora() throws Exception {
-		PlaEstudis PE = new PlaEstudis("");
-	}
-	
-	/**
-	 * Comprovem el correcte funcionament de setRangDia i els gets del rang
-	 * @throws Exception 
-	 */
-	@Test 
-	public void testSetGetRangDia() throws Exception {
-		int[] rang = new int[] {8,13,15,20};
-		PE3.setRangDia(rang);
-		assertArrayEquals(new int[] {8,13}, PE3.getRangMati());
-		assertArrayEquals(new int[] {15,20}, PE3.getRangTarda());
-	}
-	
-	/**
-	 * Comprovem el correcte funcionament de setNom i getNom
-	 */
-	@Test
-	public void getSetNom() {
-		PE2.setNom("UB");
-		assertEquals(PE2.getNom(), "UB");
-	}
-	
-	/**
-	 * Comprovem que salti error en cas de intentar violar la clau primaria de PlaEstudis
-	 */
-	@Test (expected = Exception.class)
-	public void testSetNomError() {
-		PE1.setNom("UDA");
-	}	
-	
-	/**
-	 * Comprova que funcioni donar d'alta i donar de baixa assignatures
+	 * Funció main del driver, introdueixes un número i et comprova unes quantes funcions
 	 * @throws Exception
 	 */
-	@Test
-	public void checkAssignatures() throws Exception {
-		assertEquals(PE1.altaAssignatura("PROP"), 0);
-		assertEquals(PE3.altaAssignatura("IES"), 0);
-		assertEquals(PE1.quantesAssignatures(), 1);
-		assertEquals(PE2.quantesAssignatures(), 0);
-		assertEquals(PE1.baixaAssignatura("PROP"), 0);
-		assertEquals(PE1.quantesAssignatures(), 0);
+	public static void main (String [ ] args) throws Exception {
+		System.out.println("Benvingut a l'eina de comprovació de la classe PlaEstudis");
+		PlaEstudis PE1 = new PlaEstudis("UAB");
+		System.out.println("Ben creada UAB");
+		PlaEstudis PE2 = new PlaEstudis("UPC");
+		System.out.println("Ben creada UPC");
+		PlaEstudis PE3 = new PlaEstudis("eii");
+		System.out.println("Ben creada UDA");
+		PlaEstudis PE = new PlaEstudis("fib");
+		System.out.println("Ben creada fib");
+		
+		System.out.println("Benvingut a l'eina de comprovació de la classe SessioGrup");
 	}
 }
