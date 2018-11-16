a2705cb915709ea7d8bd72a56e25cd326ab0c6f0
Hèctor Morales Carnicé <thehector1593@gmail.com>
2018-11-16 15:49:21 +0100

    Add files via upload

--- classes/ControladorDomini.java
+++ classes/ControladorDomini.java
@@ -1,11 +1,125 @@
 package classes;
 
+import classes.*;
+import java.util.*;
+import utils.*;
+
 /**
  * 
  * @author eric.casanovas@est.fib.upc.edu
+ * @autor hector.morales.carnice@est.fib.upc.edu
  *
  */
-
-public class ControladorDomini {
+public final class ControladorDomini {
+	/**
+	 * Crea, si pot, un campus amb nomCampus.
+	 * @param nomCampus Indica el nom del nou campus.
+	 * @return Un String null, si s'ha pogut crear; altrament conté el titol d'error.
+	 */
+	public String CrearCampus(String campus) {
+		try {
+			Campus newCampus = new Campus(campus);
+			
+		} catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
 	
+	public String EliminarCampus(String campus) {
+		Campus.eliminarCampus(campus);
+		return null;
+	}
+	
+	public String CrearAula(String campus, String aula, int capacitat) {
+		try {
+			Campus.getCampus(campus).altaAula(aula, capacitat);
+		}
+		catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+	
+	public String EliminarAula(String campus, String aula) {
+		Campus.getCampus(campus).baixaAula(aula);
+		return null;
+	}
+	
+	public String CrearPlaEstudis(String plaEstudis) {
+		try {
+			PlaEstudis newPlaEstudis = new PlaEstudis(plaEstudis);
+			
+		} catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+	
+	public String EliminaPlaEstudis(String plaEstudis) {
+		PlaEstudis.eliminaPlaEstudis(plaEstudis);
+		return null;
+	}
+	
+	public String CrearAssignatura(String plaEstudis, String assignatura) {
+		try {
+			PlaEstudis.getPlaEstudis(plaEstudis).altaAssignatura(assignatura);
+		}
+		catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+
+	public String EliminarAssignatura(String plaEstudis, String assignatura) {
+		try {
+			PlaEstudis.getPlaEstudis(plaEstudis).baixaAssignatura(assignatura);
+		}
+		catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+
+	public String CrearGrup(String plaEstudis, String assignatura, int grup, int capacitat) {
+		try {
+			PlaEstudis.getPlaEstudis(plaEstudis).getAssignatura(assignatura).altaGrup(grup, capacitat, "MT");
+		}
+		catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+	
+	
+	public String EliminarGrup(String plaEstudis, String assignatura, int grup) {
+		PlaEstudis.getPlaEstudis(plaEstudis).getAssignatura(assignatura).baixaGrup(grup);
+		return null;
+	}
+	
+	
+	public String CrearSubGrup(String plaEstudis, String assignatura, int grup, int subGrup, int places, boolean force) {
+		try {
+			PlaEstudis.getPlaEstudis(plaEstudis).getAssignatura(assignatura).getGrup(grup).altaSubGrup(subGrup, places, force);
+		}
+		catch(Exception e) {
+			return e.toString();
+		}
+		
+		return null;
+	}
+	
+	
+	public String EliminaSubGrup(String plaEstudis, String assignatura, int grup, int subGrup) {
+		PlaEstudis.getPlaEstudis(plaEstudis).getAssignatura(assignatura).getGrup(grup).baixaSubGrup(subGrup);
+		return null;
+	}
+	
+
 }
